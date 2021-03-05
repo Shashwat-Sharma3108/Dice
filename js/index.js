70a1f302aafdbbda0bc83c591af70bde42631a7a
Shashwat Sharma <70731419+Shashwat-Sharma3108@users.noreply.github.com>
2021-03-06 03:40:58 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var randomNumber1=Math.round(Math.random()*6);
+var randomNumber2=Math.round(Math.random()*6);
+
+if(randomNumber1==1)
+    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice1.png");
+else if(randomNumber1==2)
+    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice2.png");
+else if(randomNumber1==3)
+    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice3.png");
+else if(randomNumber1==4)
+    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice4.png");
+else if(randomNumber1==5)
+    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice5.png");
+else 
+    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice6.png");
+
+if(randomNumber2==1)
+    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice1.png");
+else if(randomNumber2==2)
+    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice2.png");
+else if(randomNumber2==3)
+    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice3.png");
+else if(randomNumber2==4)
+    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice4.png");
+else if(randomNumber2==5)
+    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice5.png");
+else 
+    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice6.png");
+
+if(randomNumber1>randomNumber2)
+    document.querySelector("h1").innerHTML="Player 1 wins 🚩";
+else if(randomNumber2>randomNumber1)
+    document.querySelector("h1").innerHTML="Player 2 wins 🚩";
+else
+    document.querySelector("h1").innerHTML="Draw!";
