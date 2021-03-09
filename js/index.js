09f401f4a682b2c770cf704214fabdef743a7d0d
Shashwat Sharma <70731419+Shashwat-Sharma3108@users.noreply.github.com>
2021-03-09 15:54:05 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomNumber2=Math.floor(Math.random()*6)+1;
+
+document.querySelector("button").addEventListener("click",Playgame);
+
+function Playgame() {
+    
+var randomNumber1=Math.round(Math.random()*6);
+var randomNumber2=Math.round(Math.random()*6);
 
 if(randomNumber1==1)
     document.getElementsByClassName("img1")[0].setAttribute("src","images/dice1.png");
@@ -33,3 +38,5 @@ else if(randomNumber2>randomNumber1)
     document.querySelector("h1").innerHTML="Player 2 wins 🚩";
 else
     document.querySelector("h1").innerHTML="Draw!";
+}
+
